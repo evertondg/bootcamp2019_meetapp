b0e2f4f8757d1eb804e4d60cd5ff7ee4a78c0565
evertondg <evertondg@hotmail.com.br>
2019-09-12 23:01:55 -0300

    feat: Adicionado rota para gravar dados dos meetups

---
+++ src/app/models/Meetup.js
@@ -0,0 +1,33 @@
+import Sequelize, { Model } from 'sequelize';
+
+class Meetup extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        title: Sequelize.STRING,
+        description: Sequelize.STRING,
+        date: Sequelize.DATE,
+        location: Sequelize.STRING,
+        user_id: Sequelize.INTEGER,
+        banner_id: Sequelize.INTEGER,
+      },
+      {
+        sequelize,
+      }
+    );
+
+    return this;
+  }
+
+  static associate(models) {
+    this.belongsTo(models.File, {
+      foreignKey: 'banner_id',
+    });
+
+    this.belongsTo(models.User, {
+      foreignKey: 'user_id',
+    });
+  }
+}
+
+export default Meetup;
